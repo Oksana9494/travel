0d9d9496c52b8474b1b49a409f99f62d862d93b2
Oksana9494 <o90494@yandex.ru>
2021-06-02 18:57:03 +0300

    changing the position of the modal window

--- js/modal.js
+++ js/modal.js
@@ -5,23 +5,20 @@
  btn.onclick = function () {
     modal.style.display = "block";
     clearTimeout(slideInterval);
-    document.body.style.overflowY = "hidden";
-    document.body.style.paddingRight = 15 + ("px ");
+    document.body.classList.add('non-scrolling');
  }
 
  span.onclick = function () {
     modal.style.display = "none";
     slideInterval = setInterval(nextSlide,3000);
-    document.body.style.overflowY = "scroll";
-    document.body.style.paddingRight = 0 + ("px ");
+    document.body.classList.remove('non-scrolling');
  }
 
  window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
         slideInterval = setInterval(nextSlide,3000);
-        document.body.style.overflowY = "scroll";
-        document.body.style.paddingRight = 0 + ("px ");
+        document.body.classList.remove('non-scrolling');
     }
 }
 
